05b036b62c1c78ecfb70f0f82c7ec3d1821f7dcf
Robin Trigon <86462148+RobinTrigon@users.noreply.github.com>
2022-07-17 13:51:25 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -44,6 +44,70 @@ for j in $(cat $payload); do
 echo "$url" | qsreplace "$j" | tee -a tempredirect.txt
 done
 for k in $(cat tempredirect.txt);do
+	echo "$k" | xargs -P30 -n1 curl -I |egrep "google.com|216.58.214.206|172.217.167.46|216.58.214.206|evil.com|gov.ai|example.com" &>/dev/null && echo -e "$k" | tee -a resulttemp.txt
+done
+clear
+banner
+cat resulttemp.txt|sort -u | tamu url
+rm resulttemp.txt
+rm tempredirect.txt
+}
+
+
+
+function multi(){
+
+
+	echo -e -n " [*] Enter your url list:"
+	read url
+
+	echo -e -n " [*] Enter your payload list:"
+	read payload
+echo -e "\n\033[1;38;5;154m [+] S C A N N I N G.."
+for i in $(cat $url ); do
+for j in $(cat $payload); do
+
+echo "$i" | qsreplace "$j" | tee -a tempredirect.txt
+done
+done
+for k in $(cat tempredirect.txt);do
+	echo "$k" | xargs -P30 -n1 curl -s -m5 -I|egrep "google.com|216.58.214.206|172.217.167.46|216.58.214.206|evil.com|gov.ai|example.com" &>/dev/null && echo -e "$k" | tee -a resulttemp.txt
+done
+clear
+banner
+cat resulttemp.txt|sort -u | tamu url >>result.txt
+cat result.txt|sort -u | tee -a screen.txt
+
+echo -e "\033[0m"
+
+echo -e "\n\033[1;38;5;154m[+]  S C R E E N S H O T I N G . . . . "
+echo -e "\033[0m"
+
+gowitness file -f screen.txt 
+echo -e "\033[0m"
+
+echo -e " \033[1;38;5;196m[+] \033[1;38;5;040m result save into \033[1;38;5;154m'result.txt' \033[1;38;5;040m and \033[1;38;5;154m'screenshots' \033[1;38;5;040m file !"
+echo -e "\033[0m"
+
+
+rm gowitness.sqlite3
+
+rm resulttemp.txt
+rm tempredirect.txt
+rm screen.txt
+
+}
+
+
+
+
+
+
+banner
+menu
+
+
+
 	echo "$k" | xargs -P30 -n1 curl -I |egrep "google.com|216.58.214.206|172.217.167.46|216.58.214.206 &>/dev/null && echo -e "$k" | tee -a resulttemp.txt
 done
 clear
